0939fff44ba3ad936e01b799f4b83b9bb3e1e08a
Mayuri Mhavale <141899038+Mayurimh@users.noreply.github.com>
2025-03-21 22:42:24 +0530

    default value of each data type

---
+++ Static modifier/DefaultValues.java
@@ -0,0 +1,24 @@
+class DefaultValues
+{
+	static int a;
+	static byte b;
+	static short c;
+	static long d;
+	static float e;
+	static double f;
+	static char g;
+	static String h;
+	static boolean i;
+	
+	public static void main(String [] args){
+		System.out.println("default value of int : "+a);
+		System.out.println("default value of byte: "+b);
+		System.out.println("default value of short: "+c);
+		System.out.println("default value of long: "+d);
+		System.out.println("default value of float: "+e);
+		System.out.println("default value of double: "+f);
+		System.out.println("default value of char: "+g);
+		System.out.println("default value of string: "+h);
+		System.out.println("default value of boolean: "+i);
+	}
+}
