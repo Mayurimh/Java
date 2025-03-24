fe965e2f383bd9d31d8e17f5989db6822824e02c
Mayuri Mhavale <141899038+Mayurimh@users.noreply.github.com>
2025-03-24 22:26:27 +0530

    Multiple classes
    Multiple classes refers to having more than one classes in one class inside a single java file is known as multiple classes. Here compiler creates one class file for every class present in java file.

---
+++ multiple classes/MultipleClasses.java
@@ -0,0 +1,20 @@
+//for save this file we use another name and after compilation we can excute different class one by one.
+class Aclass
+{
+	public static void main(String[] args){
+		System.out.println("From Class A");
+	}
+}
+
+class Bclass
+{
+	public static void main(String[] args){
+		System.out.println("From Class B");
+	}
+}
+class Cclass
+{
+	public static void main(String[] args){
+		System.out.println("From Class C");
+	}
+}
