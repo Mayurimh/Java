2cb123c03b96058cd07422c8dc7c004bf7e02f16
Mayuri Mhavale <141899038+Mayurimh@users.noreply.github.com>
2025-06-09 23:18:56 +0530

    access static- no stativ by obj reference

---
+++ OOPs concepts/Static members/X.java
@@ -0,0 +1,7 @@
+public class X
+{
+	static int a=100;
+	public static void main(String[] args){
+		System.out.println("from main method of class X");
+	}
+}
---
+++ OOPs concepts/Static members/Y.java
@@ -0,0 +1,9 @@
+public class Y extends X
+{
+	public static void test(){
+		System.out.println("From method test of class Y");
+	}
+	public void display(){
+		System.out.println(a);
+	}
+}
